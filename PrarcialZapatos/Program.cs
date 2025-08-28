d6b4e22ee1055b4145734d953553754261481f7b
Mariana <2269068@gmail.com>
2025-08-28 09:08:36 -0500

    Cambio1

--- PrarcialZapatos/Program.cs
+++ PrarcialZapatos/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+public class Tipos
+{
+    public int Id = 0;
+    public string Nombre = " ";
+}
+public class Marcas 
+{
+    public int Id = 0;
+    public String Nombre = " ";
+}
